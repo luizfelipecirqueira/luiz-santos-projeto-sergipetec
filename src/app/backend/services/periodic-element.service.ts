b62565b4deb2a337e69eae97599ede326abd09ac
luizfelipecirqueira <lfcs18ts@gmail.com>
2022-07-16 22:09:30 -0300

    Criando função para manipular erros

--- src/app/backend/services/periodic-element.service.ts
+++ src/app/backend/services/periodic-element.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
-import { Observable} from 'rxjs';
+import { HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 import { PeriodicElement } from '../models/PeriodicElement';
 
 @Injectable()
@@ -16,15 +17,39 @@ export class PeriodicElemenntService {
   }
 
   createElements(element: PeriodicElement): Observable<PeriodicElement>{
-    return this.http.post<PeriodicElement>(this.elementApiUrl, element);
+    return this.http.post<PeriodicElement>(this.elementApiUrl, element).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
   }
 
   editElement(element: PeriodicElement): Observable<PeriodicElement>{
-    return this.http.put<PeriodicElement>(this.elementApiUrl + '/' + element.id, element);
+    return this.http.put<PeriodicElement>(this.elementApiUrl + '/' + element.id, element).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
   }
 
   deleteElement(id: number): Observable<any>{
-    return this.http.delete<any>(`${this.elementApiUrl}?id=${id}`);
+    return this.http.delete<any>(`${this.elementApiUrl}?id=${id}`).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
   }
+
+
+  //Manipulando os erros
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Erro ocorreu no lado do client
+      errorMessage = error.error.message;
+    } else {
+      // Erro ocorreu no lado do servidor
+      errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  };
 
 }
